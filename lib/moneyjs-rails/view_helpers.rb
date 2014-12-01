e4527c0b9a8905b9a6b35337797f5826cbe1b29e
philister <philipp@albig.net>
2014-12-01 11:19:46 +0100

    handle blank API-responses

--- lib/moneyjs-rails/view_helpers.rb
+++ lib/moneyjs-rails/view_helpers.rb
@@ -3,6 +3,7 @@ module Moneyjs
   module ViewHelpers
     
     def moneyjs_currency_rates(options={})
+      outdated = false
       only_currencies = options[:only_currencies]
       if only_currencies.blank?
         only_currencies = Moneyjs.configuration.only_currencies
@@ -12,20 +13,40 @@ module Moneyjs
         only_currencies << 'USD'
       end
 
+
+      
+
       key = 'latest-currency-rates-'+Time.now.utc.strftime('%Y-%m-%d_%H')
+      
       latest = ::Rails.cache.read(key)
-      if latest.blank?
+      if latest.blank? or latest.rates.blank?
         latest = OpenExchangeRates::Rates.new.latest
-        ::Rails.cache.write(key,latest)
+        ::Rails.cache.write(key,latest) unless latest.blank? or latest.rates.blank?
+      end
+
+      if latest.blank? or latest.rates.blank?
+        outdated = true
+        # OER didnt deliver rates, try to get one of the latest
+        i = 0
+        while (latest.blank? || latest.rates.blank?) && i < 100
+          i+=1
+          key = 'latest-currency-rates-'+(Time.now.utc - i.hours).strftime('%Y-%m-%d_%H')
+          latest = ::Rails.cache.read(key)
+        end
       end
       
+      if latest.blank? or latest.rates.blank?
+        raise "sorry, could not retrieve exchangerates via cache nor OER-API \n" + 'latest API-response is: ' + OpenExchangeRates::Rates.new.latest.inspect
+      end
+
       if only_currencies.present?
         latest.rates.delete_if{|r| !r.in?(only_currencies)}
       end
     
       
 
-      out = "//currency-rate from #{DateTime.strptime(latest.raw_data['timestamp'].to_s,'%s')}\n"
+      out = "// currency-rates from #{DateTime.strptime(latest.raw_data['timestamp'].to_s,'%s')}\n"
+      out += "// they are maybe outdated\n" if outdated
       out +=   "fx.base = '#{latest.base}';"
       out +=  "fx.rates = #{latest.rates.to_json};"
 
